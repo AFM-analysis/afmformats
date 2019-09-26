4d57fac4ae90dc2fedb9f208cf79535219f15485
Paul Müller <dev@craban.de>
2019-09-26 17:34:38 +0200

    setup: initial skeleton

---
+++ README.rst
@@ -0,0 +1,33 @@
+afmformats
+==========
+
+|PyPI Version| |Build Status Linux| |Build Status Win| |Coverage Status| |Docs Status|
+
+Reading common atomic force microscopy (AFM) file formats.
+
+Documentation
+-------------
+
+The documentation, including the code reference and examples, is available at
+`afmformats.readthedocs.io <https://afmformats.readthedocs.io/en/stable/>`__.
+
+
+Installation
+------------
+To install the latest release, simply run:
+
+::
+
+    pip install afmformats
+
+
+.. |PyPI Version| image:: https://img.shields.io/pypi/v/afmformats.svg
+   :target: https://pypi.python.org/pypi/afmformats
+.. |Build Status Linux| image:: https://img.shields.io/travis/AFM-analysis/afmformats.svg?label=build_linux
+   :target: https://travis-ci.org/AFM-analysis/afmformats
+.. |Build Status Win| image:: https://img.shields.io/appveyor/ci/paulmueller/afmformats/master.svg?label=build_win
+   :target: https://ci.appveyor.com/project/paulmueller/afmformats
+.. |Coverage Status| image:: https://img.shields.io/codecov/c/github/AFM-analysis/afmformats/master.svg
+   :target: https://codecov.io/gh/AFM-analysis/afmformats
+.. |Docs Status| image:: https://readthedocs.org/projects/afmformats/badge/?version=latest
+   :target: https://readthedocs.org/projects/afmformats/builds/
---
+++ setup.py
@@ -0,0 +1,49 @@
+from os.path import dirname, exists, realpath
+from setuptools import setup, Extension, find_packages
+import sys
+
+author = "Paul Müller"
+authors = [author, "Shada Abuhattum"]
+description = 'reading common AFM file formats'
+name = 'afmformats'
+year = "2019"
+
+
+sys.path.insert(0, realpath(dirname(__file__))+"/"+name)
+try:
+    from _version import version  # @UnresolvedImport
+except:
+    version = "unknown"
+
+
+setup(
+    name=name,
+    author=author,
+    author_email='dev@craban.de',
+    url='https://github.com/AFM-analysis/afmformats',
+    version=version,
+    packages=find_packages(),
+    package_dir={name: name},
+    include_package_data=True,
+    license="MIT",
+    description=description,
+    long_description=open('README.rst').read() if exists('README.rst') else '',
+    install_requires=["jprops",
+                      "lmfit==0.9.5",
+                      "numpy>=1.14.0",
+                      ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    python_requires='>=3.6, <4',
+    keywords=["atomic force microscopy",
+              "mechanical phenotyping",
+              "tissue analysis"],
+    classifiers= [
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Intended Audience :: Science/Research'
+                 ],
+    platforms=['ALL'],
+    )
+
