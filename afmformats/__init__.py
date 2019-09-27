30addfa6544639fffe46177b63d0bfab9a9bcbb3
Paul Müller <dev@craban.de>
2019-09-27 09:43:49 +0200

    ci: add flake8 tests

--- afmformats/__init__.py
+++ afmformats/__init__.py
@@ -1,5 +1,4 @@
 from .metadata import MetaData  # noqa: 401
 from .formats import load_data  # noqa: 401
 
-from ._version import version as __version__
-
+from ._version import version as __version__  # noqa: 401
