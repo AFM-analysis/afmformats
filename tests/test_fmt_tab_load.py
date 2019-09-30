beb316150a4357873c91b74ee01ad44f3cf6459e
Paul Müller <dev@craban.de>
2019-09-30 09:13:00 +0200

    tests: fix tsv vs tab and flake8

--- tests/test_fmt_tab_load.py
+++ tests/test_fmt_tab_load.py
@@ -1,5 +1,4 @@
 """Test of basic opening functionalities"""
-import pathlib
 import tempfile
 
 import numpy as np
@@ -7,21 +6,21 @@ import numpy as np
 import afmformats
 
 
-def generate_tsv_file():
-    tf = tempfile.mktemp(suffix=".tsv", prefix="afmformats_test_tsv_")
+def generate_tab_file():
+    tf = tempfile.mktemp(suffix=".tab", prefix="afmformats_test_tab_")
     with open(tf, "w") as fd:
         fd.write("# force\ttip position\tsegment\r\n")
         for ii in range(100):
             fd.write("{}\t{}\t{}\r\n".format(ii*1e-9,
                                              np.pi*ii*1e-6,
-                                             ii>50))
+                                             ii > 50))
     return tf
 
 
 def test_open_simple():
-    tf = generate_tsv_file()
+    tf = generate_tab_file()
     data = afmformats.load_data(tf)[0]
-    assert np.sum(data["segment"]) == 49 
+    assert np.sum(data["segment"]) == 49
     assert np.allclose(data["force"][0], 0)
     assert np.allclose(data["force"][1], 1e-9)
     assert np.allclose(data["tip position"][1], np.pi*1e-6)
