c4a8eaf6ac204c885c4bf9000863aff8464fe341
Paul Müller <dev@craban.de>
2021-06-28 09:14:37 +0200

    ref: renamed submodule afm_fdist to mod_force_distance

--- afmformats/__init__.py
+++ afmformats/__init__.py
@@ -1,6 +1,6 @@
 from . import meta  # noqa: 401
 from .formats import find_data, load_data  # noqa: 401
 from .formats import supported_extensions  # noqa: 401
-from .afm_fdist import AFMForceDistance  # noqa: 401
+from .mod_force_distance import AFMForceDistance  # noqa: 401
 
 from ._version import version as __version__  # noqa: 401
