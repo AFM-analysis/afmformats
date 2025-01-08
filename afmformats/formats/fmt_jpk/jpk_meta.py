0dcefe0487cf60680b27e6f7c772b510d2a71e44
Eoghan O'Connell <eoghan.oconnell@mpl.mpg.de>
2025-01-08 12:04:10 +0100

    ref: migrate from pkg_resources to importlib.resources in codebase

--- afmformats/formats/fmt_jpk/jpk_meta.py
+++ afmformats/formats/fmt_jpk/jpk_meta.py
@@ -1,7 +1,6 @@
 import functools
 import json
-from pkg_resources import resource_filename
-
+import importlib.resources as importlib_resources
 
 __all__ = ["get_primary_meta_recipe",
            "get_secondary_meta_recipe",
@@ -10,15 +9,19 @@ __all__ = ["get_primary_meta_recipe",
 
 @functools.lru_cache()
 def get_primary_meta_recipe():
-    with open(resource_filename("afmformats.formats.fmt_jpk",
-                                "jpk_meta_primary.json")) as fd:
-        meta_recipe_pri = json.load(fd)
+    ref = importlib_resources.files(
+        "afmformats.formats.fmt_jpk") / "jpk_meta_primary.json"
+    with importlib_resources.as_file(ref) as path:
+        with open(path) as fd:
+            meta_recipe_pri = json.load(fd)
     return meta_recipe_pri
 
 
 @functools.lru_cache()
 def get_secondary_meta_recipe():
-    with open(resource_filename("afmformats.formats.fmt_jpk",
-                                "jpk_meta_secondary.json")) as fd:
-        meta_recipe_sec = json.load(fd)
+    ref = importlib_resources.files(
+        "afmformats.formats.fmt_jpk") / "jpk_meta_secondary.json"
+    with importlib_resources.as_file(ref) as path:
+        with open(path) as fd:
+            meta_recipe_sec = json.load(fd)
     return meta_recipe_sec
