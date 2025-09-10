ab343f43ffb8feda0490a58cb5f31dae8ee120bf
Eoghan O'Connell <eoghan.oconnell@mpl.mpg.de>
2025-09-10 12:05:59 +0200

    tests: add simple test locally

--- tests/test_fmt_chiaro_txt.py
+++ tests/test_fmt_chiaro_txt.py
@@ -1,35 +1,22 @@
 """Test NT-MDT text export format"""
 import pathlib
-import tempfile
-
 import numpy as np
-import pytest
-from nptdms import TdmsFile
 
 import afmformats
-
 
 data_path = pathlib.Path(__file__).resolve().parent / "data"
 
 
-def test_load_tdms():
-    path = r"C:\Users\eoconne\Documents\Students_Colleagues\Alice\afm\Pyjibe_implementation\Data_from_Chiaro\AEBP1_01 Indentation_002.tdms"
-    file = TdmsFile(path)
-    groups = file.groups()
-    channels = file.groups()[0].channels()
-    print("")
-
-
 def test_open_simple_txt():
-    path =pathlib.Path((r"C:\Users\eoconne\Documents\Students_Colleagues\Alice\afm"
-            r"\Pyjibe_implementation\Data_from_Chiaro"
-            r"\AEBP1_01 Indentation_002.txt"))
-
+    path = pathlib.Path((r"C:\Users\eoconne\Documents"
+                         r"\Students_Colleagues\Alice\afm"
+                         r"\Pyjibe_implementation\Data_from_Chiaro"
+                         r"\AEBP1_01 Indentation_002.txt"))
 
     data = afmformats.load_data(path=path)[0]
-    assert data["force"].size == 1644
+    assert data["force"].size == 13827
     # maximum force is at end of 1st segment
-    assert np.sum(data["segment"] == 0) == np.argmax(data["force"])+1
+    assert np.sum(data["segment"] == 0) == np.argmax(data["force"]) + 1
 
 
 if __name__ == "__main__":
